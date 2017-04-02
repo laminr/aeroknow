e6d0ced8d4c7bcedf3264a3322d0d64f78f19485
Thibault de Lambilly <tdelambilly@eventual.biz>
2017-04-02 21:08:31 +0200

    reduced NetworkMapper functions

--- app/src/main/java/biz/eventually/atpl/network/NetworkMapper.kt
+++ app/src/main/java/biz/eventually/atpl/network/NetworkMapper.kt
@@ -11,29 +11,14 @@ import biz.eventually.atpl.network.network.TopicNetwork
  * Created by thibault on 21/03/17.
  */
 
-//interface NetworkMapper {
+fun toAppSource(from: SourceNetwork) = Source(from.id, from.name)
 
-fun toAppSource(from: SourceNetwork): Source {
-    return Source(from.id, from.name)
-}
+fun toAppSources(from: List<SourceNetwork>?) = from?.map(::toAppSource)
 
-fun toAppSources(from: List<SourceNetwork>?): List<Source>? {
-    return from?.map(::toAppSource)
-}
+fun toAppSubject(from: SubjectNetwork) = Subject(from.id, from.name, from.topics)
 
-fun toAppSubject(from: SubjectNetwork): Subject {
-    return Subject(from.id, from.name, from.topics)
-}
+fun toAppSubjects(from: List<SubjectNetwork>?) = from?.map(::toAppSubject)
 
-fun toAppSubjects(from: List<SubjectNetwork>?): List<Subject>? {
-    return from?.map(::toAppSubject)
-}
+fun toAppTopic(from: TopicNetwork) = Topic(from.id, from.name, from.questions, from.follow, from.focus)
 
-fun toAppTopic(from: TopicNetwork): Topic {
-    return Topic(from.id, from.name, from.questions, from.follow, from.focus)
-}
-
-fun toAppTopics(from: List<TopicNetwork>?): List<Topic>? {
-    return from?.map(::toAppTopic)
-}
-//}
+fun toAppTopics(from: List<TopicNetwork>?) = from?.map(::toAppTopic)
