52de0c89b0de5bba66f63fc30942528f469d1828
Thibault de Lambilly <tdelambilly@eventual.biz>
2017-05-20 00:22:44 +0200

    Improvement SubjectViewHolder

--- app/src/main/java/biz/eventually/atpl/ui/subject/SubjectViewHolder.kt
+++ app/src/main/java/biz/eventually/atpl/ui/subject/SubjectViewHolder.kt
@@ -1,38 +1,32 @@
 package biz.eventually.atpl.ui.subject
 
-import android.support.v7.widget.CardView
 import android.support.v7.widget.RecyclerView
 import android.view.View
-import android.widget.TextView
-import biz.eventually.atpl.R
 import biz.eventually.atpl.data.model.dto.TopicDto
+import kotlinx.android.synthetic.main.item_subject_row.view.*
 
 /**
  * Created by Thibault de Lambilly on 29/03/2017.
  */
 class SubjectViewHolder(itemView: View, val itemClick: (TopicDto) -> Unit) : RecyclerView.ViewHolder(itemView) {
 
-    private var mTitle: TextView = itemView.findViewById(R.id.subject_title) as TextView
-    private var mCard: CardView = itemView.findViewById(R.id.topic_card) as CardView
-    private var mTopicName: TextView = itemView.findViewById(R.id.topic_item_name) as TextView
-    private var mQuestionNumber: TextView = itemView.findViewById(R.id.topic_item_questions) as TextView
-
     fun bind(topic: TopicDto) {
 
         with(topic) {
-            mTitle.text = "processing...."
+            itemView.subject_title.text = "processing...."
             if (id == -1) {
-                mTitle.text = name
+                itemView.subject_title.text = name
+                itemView.subject_title.visibility = View.VISIBLE
 
-                mTitle.visibility = View.VISIBLE
-                mCard.visibility = View.GONE
+                itemView.topic_card.visibility = View.GONE
             } else {
-                mTopicName.text = name
-                mQuestionNumber.text = questions.toString()
+                itemView.topic_item_name.text = name
+                itemView.topic_item_questions.text = questions.toString()
 
-                mTitle.visibility = View.GONE
-                mCard.visibility = View.VISIBLE
+                itemView.subject_title.visibility = View.GONE
+                itemView.topic_card.visibility = View.VISIBLE
             }
+
             itemView.setOnClickListener { itemClick(this) }
         }
     }
