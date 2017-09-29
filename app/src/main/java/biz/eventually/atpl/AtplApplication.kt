4f0c0a9a427f6ae4231ccc5ca43419a2523da77f
Thibault de Lambilly <thibault.delambilly@selsia.fr>
2017-09-29 15:33:00 +0200

    ATR vector

--- app/src/main/java/biz/eventually/atpl/AtplApplication.kt
+++ app/src/main/java/biz/eventually/atpl/AtplApplication.kt
@@ -37,14 +37,15 @@ class AtplApplication : Application() {
 
         MultiDex.install(this)
 
-        /*
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return
+        if (BuildConfig.DEBUG) {
+            if (LeakCanary.isInAnalyzerProcess(this)) {
+                // This process is dedicated to LeakCanary for heap analysis.
+                // You should not init your app in this process.
+                return
+            }
+            LeakCanary.install(this)
         }
-        LeakCanary.install(this)
-        */
+
     }
 
 }
