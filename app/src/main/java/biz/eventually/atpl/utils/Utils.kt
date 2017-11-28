d38746d67d365c9798a376225a36f298e33571b6
Thibault de Lambilly <tdelambilly@eventual.biz>
2017-11-28 08:33:42 +0100

    adding to utils

--- app/src/main/java/biz/eventually/atpl/utils/Utils.kt
+++ app/src/main/java/biz/eventually/atpl/utils/Utils.kt
@@ -8,6 +8,7 @@ import java.util.*
 
 /**
  * Created by Thibault de Lambilly on 04/04/17.
+ *
  */
 
 fun <T : Comparable<T>> shuffle(items: MutableList<T>): List<T> {
@@ -30,4 +31,19 @@ fun hasInternetConnection(): Boolean {
 
     val activeNetwork = cm.activeNetworkInfo
     return activeNetwork != null && activeNetwork.isConnectedOrConnecting
+}
+
+/**
+ * Functions that check several object for null state, then return their non null version
+ */
+fun <T1, T2> ifNotNull(value1: T1?, value2: T2?, bothNotNull: (T1, T2) -> (Unit)) {
+    if (value1 != null && value2 != null) {
+        bothNotNull(value1, value2)
+    }
+}
+
+fun <T1, T2, T3> ifNotNull(value1: T1?, value2: T2?, value3: T3?, then: (T1, T2, T3) -> (Unit)) {
+    if (value1 != null && value2 != null && value3 != null) {
+        then(value1, value2, value3)
+    }
 }
