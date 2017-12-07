b3f2d227e1b4460c80fe341ef6ed8b6de24feffc
Thibault de Lambilly <tdelambilly@eventual.biz>
2017-12-07 11:32:17 +0100

    question activity with room - wip

--- app/src/main/java/biz/eventually/atpl/data/dao/BaseDao.kt
+++ app/src/main/java/biz/eventually/atpl/data/dao/BaseDao.kt
@@ -6,17 +6,17 @@ import android.arch.persistence.room.*
  * Created by Thibault de Lambilly on 17/10/17.
  */
 @Dao
-abstract interface BaseDao<in T> {
+interface BaseDao<in T> {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    abstract fun insert(type: T): Long
+    fun insert(type: T): Long
 
     @Insert
-    abstract fun insert(vararg obj: T)
+    fun insert(vararg obj: T)
 
     @Update
-    abstract fun update(type: T)
+    fun update(type: T)
 
     @Delete
-    abstract  fun delete(type: T)
+    fun delete(type: T)
 }
